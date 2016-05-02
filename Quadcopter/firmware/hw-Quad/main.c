4c32f0eb46210c1fa897aa5c2675b2fde8fb594c
handresmar <handresmar@gmail.com>
2016-05-02 14:33:58 -0500

    Update main.c

--- Quadcopter/firmware/hw-Quad/main.c
+++ Quadcopter/firmware/hw-Quad/main.c
@@ -10,37 +10,44 @@ void buffertec_clear();
 
 int main()
 {
- int t;
-for(;;){
-switch (t)
-{
-	case 'u':
-		up();
-		break;
+	char t;
+	msleep(100);
+	init_wifi();
+	for(;;){
+		t='\0';
+		while(t =='\n' || t =='\r'|| t == ' '  || t == '\0' || t =='\v'|| t == 10 || t == 13){
+			t = wifi_getchar();
+			sttc();
+		}
+		switch (t)
+		{
+		case 'u':
+			up();
+			break;
 
-	case 'd':
-		down();
-		break;
+		case 'd':
+			down();
+			break;
 
-	case 'r':
-		right();
-		break;
+		case 'r':
+			right();
+			break;
 
-	case 'l':
-		left();
-		break;
+		case 'l':
+			left();
+			break;
 
-	case 'f':
-		forward();
-		break;
+		case 'f':
+			forward();
+			break;
 
-	case 'b':
-		backward();
-		break;
+		case 'b':
+			backward();
+			break;
 
-	default:
-		sttc();
-}
-}
+		default:
+			sttc();
+		}
+	}
 
 }
